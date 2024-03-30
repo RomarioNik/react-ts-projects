66874bc7e3684d59bfc22c589bc8c12dbea14de7
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2024-03-30 22:58:33 +0000

    first commit

---
+++ 01-birthday-reminder-ts/src/App.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import data from "./data";
+import { List } from "./List";
+
+export const App = () => {
+  return <h2>reminder project setup</h2>;
+};
---
+++ 01-birthday-reminder-ts/src/List.tsx
@@ -0,0 +1,7 @@
+export const List = () => {
+  return (
+    <>
+      <h2>list component</h2>
+    </>
+  );
+};
